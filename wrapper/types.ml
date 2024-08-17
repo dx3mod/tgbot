8f5b48f80840df9fb46474a485c4f6a4d1cbb504
dx3mod <dx3mod@bk.ru>
2024-08-17 18:54:32 +0300

    feat(api): add custom of_yojson for Update.t

--- wrapper/types.ml
+++ wrapper/types.ml
@@ -40,56 +40,56 @@ module Location = struct
   type t = { longitude : float; latitude : float } [@@deriving of_yojson, show]
 end
 
+module Message = struct
+  type t = {
+    message_id : int;
+    from : User.t option; [@default None]
+    date : int;
+    chat : Chat.t;
+    forward_from : User.t option; [@default None]
+    forward_date : int option; [@default None]
+    reply_to_message : t option; [@default None]
+    text : string option; [@default None]
+    caption : string option; [@default None]
+  }
+  [@@deriving of_yojson { strict = false }, show]
+  (* TODO: implement all fields for message type  *)
+end
+
+module InlineQuery = struct
+  type t = {
+    id : string;
+    from : User.t;
+    location : Location.t option; [@default None]
+    query : string;
+    offset : string;
+  }
+  [@@deriving of_yojson, show]
+end
+
+module ChosenInlineResult = struct
+  type t = {
+    result_id : string;
+    from : User.t;
+    location : Location.t option; [@default None]
+    inline_message_id : string option; [@default None]
+    query : string;
+  }
+  [@@deriving of_yojson, show]
+end
+
+module CallbackQuery = struct
+  type t = {
+    id : string;
+    from : User.t;
+    message : Message.t option; [@default None]
+    inline_message_id : string option; [@default None]
+    data : string;
+  }
+  [@@deriving of_yojson, show]
+end
+
 module Update = struct
-  module Message = struct
-    type t = {
-      message_id : int;
-      from : User.t option; [@default None]
-      date : int;
-      chat : Chat.t;
-      forward_from : User.t option; [@default None]
-      forward_date : int option; [@default None]
-      reply_to_message : t option; [@default None]
-      text : string option; [@default None]
-      caption : string option; [@default None]
-    }
-    [@@deriving of_yojson { strict = false }, show]
-    (* TODO: implement all fields for message type  *)
-  end
-
-  module InlineQuery = struct
-    type t = {
-      id : string;
-      from : User.t;
-      location : Location.t option; [@default None]
-      query : string;
-      offset : string;
-    }
-    [@@deriving of_yojson, show]
-  end
-
-  module ChosenInlineResult = struct
-    type t = {
-      result_id : string;
-      from : User.t;
-      location : Location.t option; [@default None]
-      inline_message_id : string option; [@default None]
-      query : string;
-    }
-    [@@deriving of_yojson, show]
-  end
-
-  module CallbackQuery = struct
-    type t = {
-      id : string;
-      from : User.t;
-      message : Message.t option; [@default None]
-      inline_message_id : string option; [@default None]
-      data : string;
-    }
-    [@@deriving of_yojson, show]
-  end
-
   type raw = {
     update_id : int;
     message : Message.t option; [@default None]
@@ -106,4 +106,21 @@ module Update = struct
     | InlineQuery of InlineQuery.t
     | ChosenInlineResult of ChosenInlineResult.t
     | CallbackQuery of CallbackQuery.t
+
+  let of_yojson json =
+    Result.map
+      (fun raw ->
+        let value =
+          match raw with
+          | { message = Some message; _ } -> Message message
+          | { inline_query = Some inline_query; _ } -> InlineQuery inline_query
+          | { chosen_inline_result = Some chosen_inline_result; _ } ->
+              ChosenInlineResult chosen_inline_result
+          | { callback_query = Some callback_query; _ } ->
+              CallbackQuery callback_query
+          | _ -> failwith "invalid Update.raw state"
+        in
+
+        { update_id = raw.update_id; value })
+      (raw_of_yojson json)
 end
