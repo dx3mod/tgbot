5de9a00f22f21517febe181d7b847b8d0e6ededa
dx3mod <dx3mod@bk.ru>
2024-08-17 17:51:03 +0300

    feat(api): add getUpdates method

--- wrapper/bot_api.ml
+++ wrapper/bot_api.ml
@@ -4,11 +4,6 @@ type t = { client : Client.t; token : string }
 
 exception Bad_request of string
 
-let bot_api_base_uri = Uri.of_string "https://api.telegram.org/"
-
-let make_meth_uri t meth =
-  Uri.with_path bot_api_base_uri (Printf.sprintf "bot%s/%s" t.token meth)
-
 let send_request ~decoder ?(max_size = 100_000) t uri =
   Eio.Switch.run @@ fun sw ->
   let resp, body = Client.post ~sw t.client (uri t) in
@@ -19,8 +14,26 @@ let send_request ~decoder ?(max_size = 100_000) t uri =
       |> Yojson.Safe.from_string |> Response.of_yojson decoder
   | _ -> failwith "not okay"
 
+module U = struct
+  let bot_api_base_uri = Uri.of_string "https://api.telegram.org/"
+
+  let make_meth_uri t meth =
+    Uri.with_path bot_api_base_uri (Printf.sprintf "bot%s/%s" t.token meth)
+
+  let add_opt_query k v uri =
+    Option.fold ~none:uri ~some:(fun v -> Uri.add_query_param uri (k, v)) v
+end
+
 module Methods = struct
+  open U
+
   let get_me t = make_meth_uri t "getMe"
+
+  let get_updates ?offset ?limit ?timeout t =
+    make_meth_uri t "getUpdates"
+    |> add_opt_query "offset" offset
+    |> add_opt_query "limit" limit
+    |> add_opt_query "timeout" timeout
 end
 
 module Types = Types
