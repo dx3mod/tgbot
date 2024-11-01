bfa0f22baf8e69b9c37b3b2363c132cf3e19c933
dx3mod <dx3mod@bk.ru>
2024-11-01 22:07:48 +0300

    Add trigger callback to dispatcher

--- lib/dispatcher.ml
+++ lib/dispatcher.ml
@@ -2,8 +2,16 @@ open Tgbot_api.Types
 
 module type S = sig
   val on_message : Message.t -> unit Lwt.t
+  val trigger : Update.value -> unit Lwt.t
 end
 
-let handler_of (module R : S) = function
+module Defaults : S = struct
+  let on_message _ = Lwt.return_unit
+  let trigger _ = Lwt.return_unit
+end
+
+let handler_of (module R : S) update =
+  Lwt.async (fun () -> R.trigger update);
+  match update with
   | Update.Message message -> R.on_message message
   | _ -> failwith "not implement yet handing this update"
